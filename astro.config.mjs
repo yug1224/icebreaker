e3c46afb109df0e81f12eb906f10f2e264901632
Yuji Yamaguchi <yuji.yamaguchi24@gmail.com>
2024-12-21 23:26:38 +0900

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import cloudflare from '@astrojs/cloudflare'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  adapter: cloudflare(),
+  adapter: cloudflare({ mode: 'directory' }),
   output: 'server',
 })
